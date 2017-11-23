785b67cec7c1a3458ddd55604baea92b7365f22f
黄建峰 <paulandcode@gmail.com>
2017-11-23 15:37:16 +0800

    Signed-off-by: 黄建峰 <paulandcode@gmail.com>

--- src/test/java/test/ServiceTest.java
+++ src/test/java/test/ServiceTest.java
@@ -11,12 +11,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 // 若配置文件在WEB_INF下,则这么写:"file:src/main/webapp/WEB-INF/applicationContext.xml"
+//需要把spring-mvc.xml中的注释junit加载配置文件报错的两句话注释掉
 @ContextConfiguration({"classpath:config/spring-*.xml"})
 public class ServiceTest {
 	
 	@Test
 	public void test1(){
-	
+		System.out.println(12);
 	}
 
 }
